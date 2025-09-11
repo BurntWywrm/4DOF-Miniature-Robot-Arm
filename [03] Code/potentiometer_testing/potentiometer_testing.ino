3edf2a8ff2862ddb37034bdc6a4e17c2e1bf0f29
Burnt <burntwyrm.work@gmail.com>
2025-09-12 09:50:31 +1200

    Completed Potentiometer Testing

--- [03] Code/potentiometer_testing/potentiometer_testing.ino
+++ [03] Code/potentiometer_testing/potentiometer_testing.ino
@@ -1,13 +1,27 @@
 // potentiometer_testing.ino
 // Code for testing a single potentiometer
 
-// Variables
-
+int potPin = A5; // initializes pot
+int potVal = 0; // stores the pot value
 
 void setup()
 {
+    Serial.begin(9600); // Starts Serial Monitor
 }
 
 void loop()
 {
+    potVal = analogRead(potPin); // reads & stores pot value
+
+    // displays the pot value
+    Serial.print("Pot Value: ");
+    Serial.println(potVal); 
+
+    int potDegrees = map(potVal, 0, 1023, 0, 180); // maps the read values into angles with a max range of 180 degrees
+    // displays the converted pot value
+    Serial.println("Pot Degrees: ");
+    Serial.print(potDegrees);
+    Serial.println(" Degrees");
+
+    delay(100); // Configure if the displayed code isnt readable
 }
