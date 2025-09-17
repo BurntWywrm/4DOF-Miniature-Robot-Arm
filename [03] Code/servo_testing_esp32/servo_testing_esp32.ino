1d964690436c6196c18ff691468be584161481a2
Burnt <burntwyrm.work@gmail.com>
2025-09-18 10:09:51 +1200

    Update servo_testing_esp32.ino

--- [03] Code/servo_testing_esp32/servo_testing_esp32.ino
+++ [03] Code/servo_testing_esp32/servo_testing_esp32.ino
@@ -8,13 +8,13 @@
 // Define maximum and minimum number of "ticks" for the servo motors.
 // Configure as necessary
 #define MIN 750 // Minimum Value for pulse width count
-#define MAX 2700 // Maximum Value for pulse widht count
+#define MAX 2700 // Maximum Value for pulse width count
 
 // Servo Motor Connectors
 // Configure as necessary
-#define SERVOL1 = 0
+#define SERVOL1 0
 
-#define FREQUENCY
+#define FREQUENCY 50
 
 // Create object to represent PCA9685 at default I2C address
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
@@ -30,4 +30,15 @@ void setup()
 
 void loop()
 {
+  // Sweeps the servo back and forth
+  for (int pulselen = MIN; pulselen <= MAX; pulselen += 5){
+    pwm.setPWM(SERVOL1, 0, pulselen);
+    delay(10);
+  }
+  delay(250); // quick pause
+  for (int pulselen = MAX; pulselen >= MIN; pulselen -= 5){
+    pwm.setPWM(SERVOL1, 0, pulselen);
+    delay(10);
+  }
+  delay(250); // quick pause
 }
